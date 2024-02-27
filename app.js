301e96f80bdacd50a32ebedc9d468ebd6fc5b3a5
eldoryaqubovebek3094 <148733238+eldoryaqubovebek3094@users.noreply.github.com>
2024-02-27 19:57:08 +0300

    changes

--- app.js
+++ app.js
@@ -13,32 +13,23 @@ app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = path.join(__dirname, 'rasmlar');
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdir(uploadDir, { recursive: true }, (err) => {
-        if (err) {
-          console.log(err);
-          return cb(err);
-        }
-        cb(null, uploadDir);
+    const uploadDir = path.join(__dirname, 'rasmlar', req.body.chatId);
+    fs.promises.mkdir(uploadDir, { recursive: true })
+      .then(() => cb(null, uploadDir))
+      .catch(err => {
+        console.error(err);
+        cb(err);
       });
-    } else {
-      cb(null, uploadDir);
-    }
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
   }
 });
+
 const upload = multer({ storage: storage });
 
-app.use(cors({ origin: 'https://yangi-hayot.netlify.app' }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://yangi-hayot.netlify.app');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());
 
 app.use('/rasmlar', express.static('rasmlar'));
 
@@ -53,162 +44,112 @@ app.get('/', (req, res) => {
 
 const token = '6887028094:AAGketUPEIU49n_9n9Q7ewljRBEoSuaRiNg';
 const bot = new TelegramBot(token, { polling: true });
-
 const rasmlarFolder = path.join(__dirname, 'rasmlar');
-if (!fs.existsSync(rasmlarFolder)) {
-  fs.mkdirSync(rasmlarFolder);
-}
+fs.promises.mkdir(rasmlarFolder, { recursive: true })
+  .catch(err => console.error(err));
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Assalomu Alaykum, iltimos, pul miqdori yozilgan chekni rasmini yuboring.');
+
+  bot.on('message', async msg => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
   
-});
-
-bot.on('message', async msg => {
-	const chatId = msg.chat.id;
-	const text = msg.text;
-
-	if (text === '/start') {
-		await bot.sendMessage(
-			chatId,
-			'Sammi.ac platformasida bor kurslarni sotib olishingiz mumkin',
-			{
-				reply_markup: {
-					keyboard: [
-						[
-							{
-								text: "Ma'lumotlarni ko'rish",
-								web_app: {
-									url: 'https://yangi-hayot.netlify.app/',
-								},
-							},
-						],
-					],
-				},
-			}
-		);
-	}
-
-	if (text === '/courses') {
-		await bot.sendMessage(
-			chatId,
-			'Sammi.ac platformasida bor kurslarni sotib olishingiz mumkin',
-			{
-				reply_markup: {
-					inline_keyboard: [
-						[
-							{
-								text: "Kurslarni ko'rish",
-								web_app: {
-									url: 'https://telegram-web-bot.vercel.app',
-								},
-							},
-						],
-					],
-				},
-			}
-		);
-	}
-
-	if (msg.web_app_data?.data) {
-		try {
-			const data = JSON.parse(msg.web_app_data?.data);
-
-			await bot.sendMessage(
-				chatId,
-				"Bizga ishonch bildirganingiz uchun raxmat, siz sotib olgan kurslarni ro'yhati"
-			);
-
-			for (item of data) {
-				await bot.sendPhoto(chatId, item.Image);
-				await bot.sendMessage(
-					chatId,
-					`${item.title} - ${item.quantity}x`
-				);
-			}
-
-			await bot.sendMessage(
-				chatId,
-				`Umumiy narx - ${data
-					.reduce((a, c) => a + c.price * c.quantity, 0)
-					.toLocaleString('en-US', {
-						style: 'currency',
-						currency: 'USD',
-					})}`
-			);
-		} catch (error) {
-			console.log(error);
-		}
-	}
-});
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
+    if (text === '/start') {
+      await bot.sendMessage(
+        chatId,
+        'Assalomu Alaykum Bot Foydalanuvchisi "Ma\'lumotlarni kiriting" degan joyga bosing va ma\'lumotlarni yuboring ✅',
+        {
+          reply_markup: {
+            keyboard: [
+              [
+                {
+                  text: "Ma'lumotlarni kiriting",
+                  web_app: {
+                    url: 'https://webmas.uz',
+                  },
+                },
+              ],
+            ],
+          },
+        }
+      );
+    }
   
+    if (text === '/izlash') {
+      await bot.sendMessage(
+        chatId,
+        'Ma\'lumotlarni kiriting',
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Ma\'lumotlarni kiriting",
+                  web_app: {
+                    url: 'https://webmas.uz',
+                  },
+                },
+              ],
+            ],
+          },
+        }
+      );
+    }
+  
+    if (msg.web_app_data?.data) {
+      try {
+        const data = JSON.parse(msg.web_app_data?.data);
+  
+        await bot.sendMessage(
+          chatId,
+          "Bizga ishonch bildirganingiz uchun raxmat"
+        );
+  
+        for (item of data) {
+          await bot.sendPhoto(chatId, item.Image);
+          await bot.sendMessage(
+            chatId,
+            `${item.title} - ${item.quantity}x`
+          );
+        }
+  
+        await bot.sendMessage(
+          chatId,
+          `Umumiy narx - ${data
+            .reduce((a, c) => a + c.price * c.quantity, 0)
+            .toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}`
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
 
 
 
-  if (msg.photo) {
-    const fileId = msg.photo[msg.photo.length - 1].file_id;
-    bot.getFileLink(fileId).then((link) => {
-      const originalFilename = getOriginalFilename(link);
-      const filename = `${chatId}_${Date.now()}_${originalFilename}`;
-      const filePath = path.join(rasmlarFolder, filename);
 
-      downloadImage(link, filePath, () => {
-        const userData = {
-          id: chatId,
-          username: msg.from.username,
-          first_name: msg.from.first_name,
-          last_name: msg.from.last_name,
-          image_name: filename,
-        };
 
-        saveUserData(userData);
-      });
-    });
-  }
-});
+
 
 function getOriginalFilename(link) {
   const urlParts = link.split('/');
   return urlParts[urlParts.length - 1];
 }
 
-function downloadImage(url, filePath, callback) {
-  axios({
-    url,
-    responseType: 'stream',
-  })
-    .then((response) => {
-      response.data.pipe(fs.createWriteStream(filePath))
-        .on('finish', () => {
-          response.data.destroy();
-          callback();
-        })
-        .on('error', (error) => {
-          console.error(`Error downloading image: ${error}`);
-        });
-    })
-    .catch((error) => {
-      console.error(`Error downloading image: ${error}`);
-    });
+async function downloadImage(url, filePath) {
+  const response = await axios({ url, responseType: 'stream' });
+  response.data.pipe(fs.createWriteStream(filePath));
 }
 
-function saveUserData(userData) {
+async function saveUserData(userData) {
   const dataFilePath = path.join(__dirname, 'data.json');
-  let data = [];
   try {
-    if (fs.existsSync(dataFilePath)) {
-      const dataContent = fs.readFileSync(dataFilePath, 'utf8');
-      if (dataContent) {
-        data = JSON.parse(dataContent);
-      }
-    }
+    const dataContent = await fs.promises.readFile(dataFilePath, 'utf8');
+    const data = dataContent ? JSON.parse(dataContent) : [];
     data.push(userData);
-    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+    await fs.promises.writeFile(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
   } catch (error) {
     console.error(`Error saving user data: ${error}`);
   }
@@ -217,3 +158,4 @@ function saveUserData(userData) {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
